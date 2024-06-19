9a2146f8e0de8db27e083e0941c3cf36eb4ee609
ricardomarinpalacios <ricardo.marin@havasit.com>
2024-06-19 10:41:49 -0400

    add: karma config para test
    Se añade karma config para la implementacion en azure

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/your-project-name'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--disable-dev-shm-usage',
+          '--remote-debugging-port=9222',
+          '--disable-setuid-sandbox',
+          '--disable-software-rasterizer',
+        ],
+      },
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
